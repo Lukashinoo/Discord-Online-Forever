f73bf64d96655c64a08a0efe8eaca9c9844d47ab
Lukashinoo <Lukas11wilhelm@gmail.com>
2025-02-17 13:03:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
+bot.on("ready", () => {
+    console.log("Bot ist online!");
+    bot.editStatus("idle", {
+        name: "Currently offline",
+        type: 3 // 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching
+    });
+});
+
 bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+    console.error(err); // or your preferred logger
 });
 
 bot.connect(); // Get the bot to connect to Discord
