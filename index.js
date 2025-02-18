5a125f8343092388842e414a00e7f3cfe4fe473e
Lukashinoo <Lukas11wilhelm@gmail.com>
2025-02-18 10:26:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ bot.on("ready", () => {
     
     // Setzt den Status auf "dnd" mit benutzerdefiniertem Status-Text
     bot.editStatus("dnd", {
-        name: "Currently offline", // Benutzerdefinierter Status-Text
-        type: 2 // 0 = spielt, 1 = streamt, 2 = hört zu, 3 = schaut, 5 = konkurriert
+        status: "Currently offline" // Benutzerdefinierter Status-Text
     });
 });
 
